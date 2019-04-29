353b6381f0a66ae6bf493070fad30c50aeb0dc75
Eugene Koontz <ekoontz@hiro-tan.org>
2019-04-28 18:28:28 -0700

    more cleanup

--- src/scramble/core.cljs
+++ src/scramble/core.cljs
@@ -5,18 +5,14 @@
    [reagent.debug :as d])
   (:import [goog.events EventType]))
 
-(defonce timer (r/atom (js/Date.)))
-
-(defonce sentence (r/atom "Io ho dato il libro interessante a Paola."))
-
-(def time-updater (js/setInterval
-                   #(reset! timer (js/Date.)) 1000))
-
 (defn tokenize [sentence]
   (clojure.string/split sentence #"[ ]+"))
 
-(def tokens (r/atom (tokenize @sentence)))
-
+(defonce timer (r/atom (js/Date.)))
+(defonce sentence (r/atom "Io ho dato il libro interessante a Paola."))
+(defonce tokens (r/atom (tokenize @sentence)))
+(def time-updater (js/setInterval
+                   #(reset! timer (js/Date.)) 1000))
 (defn set-tokens [sentence]
   (reset! tokens (tokenize sentence)))
 
@@ -25,28 +21,19 @@
 
 (declare clock)
 
-(defn on-drag [x y]
-  (d/log (str "on-drag at: " x "," y)))
-
-(defn set-sentence [sentence-input-element]
-  (reset! sentence (-> sentence-input-element .-target .-value))
-  (set-tokens @sentence))
-
 (defn on-mouse-down [drag-element]
   (let [drag-move
         (do
           (d/log (str "starting to drag word: " (-> drag-element .-target .-innerHTML)))
           (fn [evt]
-            (on-drag (.-clientX evt) (.-clientY evt))))
+            (if false (d/log (str "drag-in-progress: " (.-clientX evt) (.-clientY evt))))))
         drag-end-atom (atom nil)
-        on-start (fn []
-                   (d/log (str "dragging has started for: " (-> drag-element .-target .-innerHTML))))       drag-end
+        drag-end
         (fn [evt]
           (do
             (d/log (str "done dragging.. " (.-clientX evt) ", " (.-clientY evt)))
             (events/unlisten js/window EventType.MOUSEMOVE drag-move)
             (events/unlisten js/window EventType.MOUSEUP @drag-end-atom)))]
-    (on-start)
     (reset! drag-end-atom drag-end)
     (events/listen js/window EventType.MOUSEMOVE drag-move)
     (events/listen js/window EventType.MOUSEUP drag-end)))
@@ -57,7 +44,9 @@
    [:input {:type "text"
             :size 75
             :value @sentence
-            :on-change set-sentence}]])
+            :on-change (fn [element]
+                         (reset! sentence (-> element .-target .-value))
+                         (set-tokens @sentence))}]])
 
 (declare scrambled-word)
 (declare sentence-word)
